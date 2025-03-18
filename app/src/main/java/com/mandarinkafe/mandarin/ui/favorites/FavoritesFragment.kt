b84b4a818c4f52827f6602b584b4f3bf4359d4e7
SergeySh97 <Sergey_Sh97@mail.ru>
2025-03-18 11:09:02 +0300

    Создать фрагмент избранное

---
+++ app/src/main/java/com/mandarinkafe/mandarin/ui/favorites/FavoritesFragment.kt
@@ -0,0 +1,18 @@
+package com.mandarinkafe.mandarin.ui.favorites
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.mandarinkafe.mandarin.R
+
+class FavoritesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_favorites, container, false)
+    }
+}
