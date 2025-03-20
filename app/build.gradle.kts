de816adb568ccf4d85f3176bf21dc1f6dcd963db
SergeySh97 <Sergey_Sh97@mail.ru>
2025-03-20 13:09:14 +0300

    Добавление Hilt, Удаление Koin, Обновелние Kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.navigation.safeargs.kotlin)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -42,30 +44,65 @@ android {
 
 dependencies {
 
-    implementation(libs.adapterdelegates.dsl)// AdapterDelegates
-    implementation(libs.adapterdelegates.dsl.viewbinding)//  модуль AdapterDelegates для работы с ViewBinding
+    // AdapterDelegates
+    implementation(libs.adapterdelegates.dsl)
+    // AdapterDelegates для работы с ViewBinding
+    implementation(libs.adapterdelegates.dsl.viewbinding)
 
+    // Navigation
+    implementation(libs.androidx.navigation.fragment)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+
+    // Fragment
     implementation(libs.androidx.fragment.ktx)
+
+    // ViewPager2
     implementation(libs.androidx.viewpager2)
-    implementation(libs.koin.android)
+
+    // Hilt
+    implementation(libs.com.hilt)
+    ksp(libs.com.hilt.ksp)
+
+    // Glide
     implementation(libs.glide)
-    implementation(libs.circleindicator) //DotsIndicator
-    implementation(libs.androidx.coordinatorlayout)
-    annotationProcessor(libs.compiler)
+    ksp(libs.compiler)
+
+    // DotsIndicator
+    implementation(libs.circleindicator)
+
+    // Retrofit
     implementation(libs.retrofit)
+
+    // Gson
     implementation(libs.converter.gson)
+
+    // Core
     implementation(libs.androidx.core.ktx)
+
+    // AppCompat
     implementation(libs.androidx.appcompat)
+
+    // Material
     implementation(libs.material)
+
+    // Activity
     implementation(libs.androidx.activity)
+
+    // ConstraintLayout
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.navigation.fragment)
+
+    // CoordinatorLayout
+    implementation(libs.androidx.coordinatorlayout)
+
+    // Legacy
     implementation(libs.androidx.legacy.support.v4)
+
+    // Lifecycle
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    implementation(libs.androidx.fragment.ktx)
+
+    // JUnit
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
