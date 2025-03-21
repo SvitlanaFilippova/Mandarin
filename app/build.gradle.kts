02df941e75fa704842d7370f9fce9f76bf1a9df5
SergeySh97 <Sergey_Sh97@mail.ru>
2025-03-21 12:37:19 +0300

    Добавление navigation compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.androidx.navigation.fragment)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.navigation.compose)
 
     // Fragment
     implementation(libs.androidx.fragment.ktx)
