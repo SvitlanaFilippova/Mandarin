559d18770ca3f78485eb9c3f1f61a8f7e3ba0df7
Svitlana Filippova <s.rubinets@gmail.com>
2025-03-26 06:13:56 +0300

    Добавлен плейсхолдер для изображений блюд

--- app/src/main/java/com/mandarinkafe/mandarin/menu/ui/components/ItemMenuMeal.kt
+++ app/src/main/java/com/mandarinkafe/mandarin/menu/ui/components/ItemMenuMeal.kt
@@ -29,7 +29,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -74,20 +73,13 @@ fun ItemMenuMeal(meal: Meal) {
         verticalAlignment = Alignment.Top,
         modifier = Modifier.padding(Dimens.MarginSmall8)
     ) {
-        // временое мок-изображение для Preview. В дальенйшем будет AsyncImage
-//        Image(
-//            painter = painterResource(R.drawable.margaritta_veg),
-//            contentDescription = "Превью изображения",
-//            modifier = Modifier
-//                .size(Dimens.MealImage136)
-//                .clip(shape = RectangleShape),
-//            contentScale = ContentScale.Crop
-//        )
+
         AsyncImage(
             model = meal.imageUrl.ifEmpty { R.drawable.logo_orange },
             contentDescription = "Изображение ${meal.name}",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
+            error = painterResource(R.drawable.logo_orange),
+            placeholder = painterResource(R.drawable.logo_orange),
+                 modifier = Modifier
                 .size(Dimens.MealImage136)
                 .clip(shape = RectangleShape),
         )
