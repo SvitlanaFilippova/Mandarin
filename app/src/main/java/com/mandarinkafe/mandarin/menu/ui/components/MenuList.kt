42e27865ef04b8cdbf07b106189115e2e4e3b957
Svitlana Filippova <s.rubinets@gmail.com>
2025-04-04 11:33:05 +0300

    Добавила отображений описания категорий

--- app/src/main/java/com/mandarinkafe/mandarin/menu/ui/components/MenuList.kt
+++ app/src/main/java/com/mandarinkafe/mandarin/menu/ui/components/MenuList.kt
@@ -2,18 +2,16 @@ package com.mandarinkafe.mandarin.menu.ui.components
 
 import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.mandarinkafe.mandarin.core.ui.theme.Dimens
-import com.mandarinkafe.mandarin.core.ui.theme.Typography
 import com.mandarinkafe.mandarin.menu.domain.models.Meal
 import com.mandarinkafe.mandarin.menu.domain.models.MenuRVItem
+import com.mandarinkafe.mandarin.menu.ui.components.tabs.MenuHeaderItem
+import com.mandarinkafe.mandarin.menu.ui.components.tabs.MenuSubHeaderItem
 import com.mandarinkafe.mandarin.util.RVItem
 
 @Composable
@@ -33,27 +31,12 @@ fun MenuList(
     ) {
         itemsIndexed(menuItems) { index, item ->
             when (item) {
-                is MenuRVItem.HeaderItem -> Text(
-                    text = item.categoryName,
-                    style = Typography.MenuCategoryStyle,
-                    modifier = Modifier.padding(
-                        start = Dimens.MarginSmall8,
-                        top = Dimens.MarginBig32
-                    )
-                )
+                is MenuRVItem.HeaderItem -> MenuHeaderItem(item)
 
                 is MenuRVItem.SubHeaderItem -> {
                     val previousItem = if (index > 0) menuItems[index - 1] else null
                     val hasHeaderBefore = previousItem is MenuRVItem.HeaderItem
-                    Text(
-                        text = item.categoryName,
-                        style = Typography.MenuSubCategoryStyle,
-                        modifier = Modifier.padding(
-                            start = Dimens.MarginSmall8,
-                            top = if (!hasHeaderBefore) Dimens.MarginStandard16 else 0.dp,
-                            bottom = Dimens.MarginSmall8
-                        )
-                    )
+                    MenuSubHeaderItem(item, hasHeaderBefore)
                 }
 
                 is MenuRVItem.MealItem -> MenuMealItem(
