75af595638caafe26f93b88f6c3d94b5e59b8f5f
Svitlana Filippova <s.rubinets@gmail.com>
2025-04-22 09:08:24 +0300

    Добавлены документационные комментарии к полям Meal, чтобы не путаться в свойствах

--- app/src/main/java/com/mandarinkafe/mandarin/menu/domain/models/Meal.kt
+++ app/src/main/java/com/mandarinkafe/mandarin/menu/domain/models/Meal.kt
@@ -8,8 +8,24 @@ data class Meal(
     val price: Int,
     val imageUrl: String,
     var isFavorite: Boolean,
+
+    /**
+    Внутренние теги для особой обработки блюда в меню
+     */
     val tags: List<Tag>,
+
+    /**
+    Ярлыки для от ображения в меню и для фильтрации
+     */
     val labels: List<Label>,
+
+    /**
+    Показывает, должно ли блюдо отображаться в общем меню
+     */
     val isHidden: Boolean,
+
+    /**
+    Показывает, должен ли открываться дополнительный экран с кастомизацией блюда
+     */
     val isEditable: Boolean
 )
