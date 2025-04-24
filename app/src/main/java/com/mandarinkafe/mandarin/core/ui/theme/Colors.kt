8124f594cefd34abe12b5ba1ebcc68068048a8b9
Svitlana Filippova <s.rubinets@gmail.com>
2025-04-24 07:45:41 +0300

    Удалены лишние ресурсы для xml

--- app/src/main/java/com/mandarinkafe/mandarin/core/ui/theme/Colors.kt
+++ app/src/main/java/com/mandarinkafe/mandarin/core/ui/theme/Colors.kt
@@ -17,4 +17,18 @@ object Colors {
     val GreyTransparent75 = Color(0xBFAEAFB4)
     val WhiteTransparent75 = Color(0xBFFFFFFF)
     val Transparent = Color(0x00000000)
+
+    // Цвета областей доставки
+    val FirstArea = Color(0xAA0B7B6B)
+    val SecondArea = Color(0xAA195A42)
+    val ThirdArea = Color(0xAA376946)
+    val FourthArea = Color(0xAA637334)
+    val FifthArea = Color(0xAA978829)
+    val SixthArea = Color(0xAA9C7345)
+    val SeventhArea = Color(0xAA8D3A34)
+    val EighthArea = Color(0xAA8A1832)
+    val NinthArea = Color(0xAA83155C)
+    val TenthArea = Color(0xAA482366)
+    val EleventhArea = Color(0xAA214464)
+    val TwelfthArea = Color(0xAA2E2B70)
 }
