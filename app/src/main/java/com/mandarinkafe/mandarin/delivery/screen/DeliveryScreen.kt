2402e5b28ece5838f4b669b233e45acfb27f5a98
SergeySh97 <Sergey_Sh97@mail.ru>
2025-04-28 10:18:57 +0300

    Добавлен скролл на текст

--- app/src/main/java/com/mandarinkafe/mandarin/delivery/screen/DeliveryScreen.kt
+++ app/src/main/java/com/mandarinkafe/mandarin/delivery/screen/DeliveryScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -29,6 +31,7 @@ fun DeliveryScreen() {
     ) {
         Column(
             modifier = Modifier
+                .verticalScroll(rememberScrollState())
                 .weight(1f)
                 .padding(horizontal = Dimens.MarginStandard16)
         ) {
