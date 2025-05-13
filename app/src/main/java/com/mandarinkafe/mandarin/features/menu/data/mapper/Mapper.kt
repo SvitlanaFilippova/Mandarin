2158c7da7f6abef982840c9962842c25365a3153
Svitlana Filippova <s.rubinets@gmail.com>
2025-05-13 10:57:53 +0300

    Выполнен фикс маппера CategoryDto с учётом возможных пробелов в конце названий категорий

--- app/src/main/java/com/mandarinkafe/mandarin/features/menu/data/mapper/Mapper.kt
+++ app/src/main/java/com/mandarinkafe/mandarin/features/menu/data/mapper/Mapper.kt
@@ -18,106 +18,107 @@ import com.mandarinkafe.mandarin.util.Constants.TAG_WOK_CONSTRUCTOR
 import com.mandarinkafe.mandarin.util.applyTypography
 
 fun CategoryDto.toDomain(storedFavorites: List<String>): MealCategory {
-        val categoryLabels = labels?.map { it.toDomain() } ?: emptyList()
-        val categoryTags = tags?.map { it.toDomain() } ?: emptyList()
+    val categoryLabels = labels?.map { it.toDomain() } ?: emptyList()
+    val categoryTags = tags?.map { it.toDomain() } ?: emptyList()
 
-        val safeItems = items?.mapNotNull {
-            it.toDomain(
-                storedFavorites = storedFavorites,
-                categoryLabels = categoryLabels,
-                categoryTags = categoryTags
-            )
-        } ?: emptyList()
-
-        return MealCategory(
-            id = id,
-            name = name.applyTypography(),
-            meals = safeItems,
-            subCategories = null,
-            tabIcon = buttonImageUrl,
-            description = (description ?: "").applyTypography(),
-            isHidden = isHidden == true,
+    val safeItems = items?.mapNotNull {
+        it.toDomain(
+            storedFavorites = storedFavorites,
+            categoryLabels = categoryLabels,
+            categoryTags = categoryTags
         )
-    }
+    } ?: emptyList()
 
-    private fun MealDto.toDomain(
-        storedFavorites: List<String>,
-        categoryLabels: List<Label>,
-        categoryTags: List<Tag>
-    ): Meal? {
-        val firstSize = itemSizes?.firstOrNull()
-        val safeWeight = firstSize?.portionWeightGrams?.toInt() ?: 0
-        val safePrice = firstSize?.prices?.firstOrNull()?.price?.toInt() ?: 0
-        val safeModifiers = firstSize?.itemModifierGroups?.map { it.toDomain() } ?: emptyList()
-        val safeImageUrl = firstSize?.buttonImageUrl ?: ""
-
-        val mealLabels = (labels ?: emptyList()).map { it.toDomain() }
-        val mealTags = (tags ?: emptyList()).map { it.toDomain() }
-        val finalMealTags = (mealTags + categoryTags).distinctBy { it.name }
-        val finalMealLabels = (mealLabels + categoryLabels).distinctBy { it.name }
-
-        return Meal(
-            id = itemId,
-            name = name.applyTypography(),
-            description = (description ?: "").applyTypography(),
-            weight = safeWeight,
-            price = safePrice,
-            imageUrl = safeImageUrl,
-            isFavorite = storedFavorites.contains(itemId),
-            labels = finalMealLabels,
-            tags = finalMealTags,
-            isHidden = isHidden == true,
-            modifiers = safeModifiers,
-            editableType = checkMealType(finalMealTags, safeModifiers),
-        )
-    }
-
-    private fun checkMealType(tags: List<Tag>, modifiers: List<ModifierGroup>): EditableType? {
-        if (tags.any { it.name.equals(TAG_PIZZA_ADDS, ignoreCase = true) }) {
-            return EditableType.PIZZA
-        } else
-            if (tags.any { it.name.equals(TAG_WOK_CONSTRUCTOR, ignoreCase = true) }) {
-                return EditableType.WOK
-            } else if (modifiers.isNotEmpty()) {
-                return EditableType.MODIFIABLE
-            } else
-                return null
-    }
-
-
-    fun CategoryDto.hasParent(): Boolean =
-        name.contains("/")
-
-    fun CategoryDto.parentName(): String =
-        name.substringBefore("/")
-
-    fun CategoryDto.subName(): String =
-        name.substringAfter("/")
-
-    fun TagDto.toDomain() = Tag(
+    return MealCategory(
         id = id,
-        name = name
+        name = name.applyTypography(),
+        meals = safeItems,
+        subCategories = null,
+        tabIcon = buttonImageUrl,
+        description = (description ?: "").applyTypography(),
+        isHidden = isHidden == true,
     )
+}
 
-    fun LabelDto.toDomain() = Label(
-        id = code,
-        name = name
+private fun MealDto.toDomain(
+    storedFavorites: List<String>,
+    categoryLabels: List<Label>,
+    categoryTags: List<Tag>
+): Meal? {
+    val firstSize = itemSizes?.firstOrNull()
+    val safeWeight = firstSize?.portionWeightGrams?.toInt() ?: 0
+    val safePrice = firstSize?.prices?.firstOrNull()?.price?.toInt() ?: 0
+    val safeModifiers = firstSize?.itemModifierGroups?.map { it.toDomain() } ?: emptyList()
+    val safeImageUrl = firstSize?.buttonImageUrl ?: ""
+
+    val mealLabels = (labels ?: emptyList()).map { it.toDomain() }
+    val mealTags = (tags ?: emptyList()).map { it.toDomain() }
+    val finalMealTags = (mealTags + categoryTags).distinctBy { it.name }
+    val finalMealLabels = (mealLabels + categoryLabels).distinctBy { it.name }
+
+    return Meal(
+        id = itemId,
+        name = name.applyTypography(),
+        description = (description ?: "").applyTypography(),
+        weight = safeWeight,
+        price = safePrice,
+        imageUrl = safeImageUrl,
+        isFavorite = storedFavorites.contains(itemId),
+        labels = finalMealLabels,
+        tags = finalMealTags,
+        isHidden = isHidden == true,
+        modifiers = safeModifiers,
+        editableType = checkMealType(finalMealTags, safeModifiers),
     )
+}
 
-    fun ModifierItemDto.toDomain(): ModifierItem {
-        val safePrice = prices?.firstOrNull()?.price?.toInt() ?: 0
+private fun checkMealType(tags: List<Tag>, modifiers: List<ModifierGroup>): EditableType? {
+    if (tags.any { it.name.equals(TAG_PIZZA_ADDS, ignoreCase = true) }) {
+        return EditableType.PIZZA
+    } else
+        if (tags.any { it.name.equals(TAG_WOK_CONSTRUCTOR, ignoreCase = true) }) {
+            return EditableType.WOK
+        } else if (modifiers.isNotEmpty()) {
+            return EditableType.MODIFIABLE
+        } else
+            return null
+}
 
-        return ModifierItem(
-            id = itemId,
-            name = name ?: "",
-            price = safePrice,
-        )
-    }
+fun CategoryDto.hasParent(): Boolean =
+    name.contains("/")
 
-    fun ModifierGroupDto.toDomain() = ModifierGroup(
-        id = itemGroupId,
+fun CategoryDto.parentName(): String =
+    name.substringBefore("/")
+        .trim()
+
+fun CategoryDto.subName(): String =
+    name.substringAfter("/")
+        .trim()
+
+fun TagDto.toDomain() = Tag(
+    id = id,
+    name = name
+)
+
+fun LabelDto.toDomain() = Label(
+    id = code,
+    name = name
+)
+
+fun ModifierItemDto.toDomain(): ModifierItem {
+    val safePrice = prices?.firstOrNull()?.price?.toInt() ?: 0
+
+    return ModifierItem(
+        id = itemId,
         name = name ?: "",
-        items = items?.map { it.toDomain() } ?: emptyList(),
-        isSingleChoice = (restrictions?.maxQuantity == 1),
+        price = safePrice,
     )
+}
+
+fun ModifierGroupDto.toDomain() = ModifierGroup(
+    id = itemGroupId,
+    name = name ?: "",
+    items = items?.map { it.toDomain() } ?: emptyList(),
+    isSingleChoice = (restrictions?.maxQuantity == 1),
+)
 
