b8ea25e7931990f881ac5592bae5c62633604d6d
Svitlana Filippova <s.rubinets@gmail.com>
2025-05-23 12:06:32 +0300

    доработка SplashScreen

--- app/src/main/java/com/mandarinkafe/mandarin/core/ui/theme/Dimens.kt
+++ app/src/main/java/com/mandarinkafe/mandarin/core/ui/theme/Dimens.kt
@@ -11,14 +11,12 @@ object Dimens {
     val CornerRadius16 = 16.dp
     val BottomBarHeight64 = 64.dp
     val Elevation2 = 2.dp
-    val ButtonBorder1 = 1.dp
     val ButtonToCartSmall32 = 32.dp
     val ButtonBox32 = 32.dp
     val ButtonToCartBig120 = 120.dp
     val ButtonsRowWidth164 = 164.dp
     val ButtonPlaceholderSize200 = 200.dp
-    val SplashScreenLogoSize180 = 180.dp
-    val SplashScreenBackSize240 = 240.dp
+    val SplashScreenLogoSize150 = 150.dp
     val IconSize24 = 24.dp
     val MarginStandard16 = 16.dp
     val Margin12 = 12.dp
--- app/src/main/java/com/mandarinkafe/mandarin/features/splash/ui/SplashScreen.kt
+++ app/src/main/java/com/mandarinkafe/mandarin/features/splash/ui/SplashScreen.kt
@@ -3,11 +3,11 @@ package com.mandarinkafe.mandarin.features.splash.ui
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -18,13 +18,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.mandarinkafe.mandarin.R
 import com.mandarinkafe.mandarin.core.ui.theme.Colors
 import com.mandarinkafe.mandarin.core.ui.theme.Dimens
+import com.mandarinkafe.mandarin.features.splash.ui.model.SplashElement
 import com.mandarinkafe.mandarin.features.splash.ui.view_model.SplashViewModel
 import com.mandarinkafe.mandarin.util.Constants.ANIMATION_DURATION_FAST
 import com.mandarinkafe.mandarin.util.Constants.SPLASH_ANIMATION_DURATION
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @Composable
@@ -32,44 +35,27 @@ fun SplashScreen(
     viewModel: SplashViewModel = hiltViewModel(),
     onFinished: () -> Unit
 ) {
-
     val state by viewModel.state.collectAsState()
-    val bgScale = remember { Animatable(2.0f) }
-    val bgAlpha = remember { Animatable(0f) }
-    val bgRotation = remember { Animatable(0f) }
+    val globalAlpha = remember { Animatable(1f) }
     val logoAlpha = remember { Animatable(1f) }
-
-    // Анимация появления (фон крутится и уменьшается)
+    // Анимация появления лого
     LaunchedEffect(Unit) {
         launch {
-            bgScale.animateTo(
-                targetValue = 1f,
-                animationSpec = tween(
-                    durationMillis = SPLASH_ANIMATION_DURATION,
-                    easing = FastOutSlowInEasing
-                )
-            )
-        }
-        launch {
-            bgAlpha.animateTo(
+            logoAlpha.animateTo(
                 targetValue = 1f,
                 animationSpec = tween(durationMillis = SPLASH_ANIMATION_DURATION)
             )
         }
-        launch {
-            bgRotation.animateTo(
-                targetValue = 360 * 2f,
-                animationSpec = tween(
-                    durationMillis = SPLASH_ANIMATION_DURATION,
-                    easing = FastOutSlowInEasing
-                )
-            )
-        }
     }
-    // Анимация исчезновения логотипа перед завершением
+
+    // Анимация скрытия всего контента
     LaunchedEffect(state.isVisible) {
         if (!state.isVisible) {
-            bgAlpha.animateTo(
+            globalAlpha.animateTo(
+                targetValue = 0f,
+                animationSpec = tween(durationMillis = ANIMATION_DURATION_FAST)
+            )
+            logoAlpha.animateTo(
                 targetValue = 0f,
                 animationSpec = tween(durationMillis = ANIMATION_DURATION_FAST)
             )
@@ -77,39 +63,132 @@ fun SplashScreen(
         }
     }
 
+    val elements = listOf(
+        SplashElement(
+            R.drawable.splash_wave2, Alignment.TopEnd, offsetX = 200f, offsetY = 500f,
+            targetOffsetX = 0f, targetOffsetY = 400f
+        ),
+        SplashElement(
+            R.drawable.splash_pizza, Alignment.TopEnd, offsetX = 400f, offsetY = 300f,
+            targetOffsetX = 100f, targetOffsetY = -100f
+        ),
+
+        SplashElement(
+            R.drawable.splash_wave2, Alignment.BottomStart, offsetX = 200f, offsetY = 300f,
+            targetOffsetX = -100f, targetOffsetY = 100f
+        ),
+
+        SplashElement(
+            R.drawable.splash_pizza3, Alignment.BottomStart, offsetX = 100f, offsetY = 000f,
+            targetOffsetX = 100f, targetOffsetY = -100f
+        ),
+
+        SplashElement(
+            R.drawable.splash_wave3, Alignment.TopStart, offsetX = -400f, offsetY = -600f,
+            targetOffsetX = 00f, targetOffsetY = 300f
+        ),
+
+        SplashElement(
+            R.drawable.splash_olives, Alignment.CenterStart, offsetX = -400f, offsetY = -600f,
+            targetOffsetX = -150f, targetOffsetY = -400f
+        ),
+
+        SplashElement(
+            R.drawable.splash_mushroom, Alignment.CenterStart, offsetX = -200f, offsetY = -400f,
+            targetOffsetX = 0f, targetOffsetY = 200f
+        ),
+
+        SplashElement(
+            R.drawable.splash_wave3, Alignment.BottomEnd, offsetX = 200f, offsetY = 500f,
+            targetOffsetX = 150f, targetOffsetY = 200f
+        ),
+
+        SplashElement(
+            R.drawable.splash_board, Alignment.BottomEnd, offsetX = 200f, offsetY = -300f,
+            targetOffsetX = 100f, targetOffsetY = 0f
+        ),
+        SplashElement(
+            R.drawable.splash_fork, Alignment.BottomEnd, offsetX = -100f, offsetY = 200f,
+            targetOffsetX = 0f, targetOffsetY = 100f
+        ),
+
+        SplashElement(
+            R.drawable.splash_tomato, Alignment.CenterEnd, offsetX = 200f, offsetY = 500f,
+            targetOffsetX = 0f, targetOffsetY = -200f
+        ),
+
+        SplashElement(
+            R.drawable.splash_wave4, Alignment.CenterEnd, offsetX = 500f, offsetY = 200f,
+            targetOffsetX = 100f, targetOffsetY = 0f
+        ),
+        SplashElement(
+            R.drawable.splash_pepper, Alignment.CenterEnd, offsetX = 200f, offsetY = 500f,
+            targetOffsetX = -80f, targetOffsetY = 300f
+        ),
+        SplashElement(
+            R.drawable.splash_cheese4, Alignment.TopStart, offsetX = -400f, offsetY = -300f,
+            targetOffsetX = -50f, targetOffsetY = -50f
+        )
+    )
 
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(Colors.AppBlack)
     ) {
-        // Фон
-        Icon(
+        elements.forEachIndexed { index, element ->
+            val offsetX = remember { Animatable(element.offsetX) }
+            val offsetY = remember { Animatable(element.offsetY) }
+
+            LaunchedEffect(Unit) {
+                launch {
+                    delay(index * 80L)
+                    offsetX.animateTo(
+                        element.targetOffsetX,
+                        tween(SPLASH_ANIMATION_DURATION, easing = FastOutSlowInEasing)
+                    )
+                }
+                launch {
+                    delay(index * 80L)
+                    offsetY.animateTo(
+                        element.targetOffsetY,
+                        tween(SPLASH_ANIMATION_DURATION, easing = FastOutSlowInEasing)
+                    )
+                }
+            }
+
+            Image(
+                painter = painterResource(id = element.resId),
+                contentDescription = null,
+                modifier = Modifier
+                    .align(element.alignment)
+                    .graphicsLayer {
+                        translationX = offsetX.value
+                        translationY = offsetY.value
+                        alpha = globalAlpha.value
+                    }
+            )
+        }
+
+        // Фон под лого
+        Image(
             painter = painterResource(id = R.drawable.background_for_logo),
-            contentDescription = stringResource(R.string.logo_cafe),
+            contentDescription = null,
             modifier = Modifier
                 .align(Alignment.Center)
-                .size(Dimens.SplashScreenBackSize240)
-                .graphicsLayer {
-                    scaleX = bgScale.value
-                    scaleY = bgScale.value
-                    rotationZ = bgRotation.value
-                    alpha = bgAlpha.value
-                },
-            tint = Colors.Orange
-        )
+                .size(200.dp)
+                .graphicsLayer { alpha = logoAlpha.value },
+
+            )
 
         // Логотип
-        Icon(
+        Image(
             painter = painterResource(id = R.drawable.logo_orange),
             contentDescription = stringResource(R.string.logo_cafe),
             modifier = Modifier
                 .align(Alignment.Center)
-                .size(Dimens.SplashScreenLogoSize180)
-                .graphicsLayer {
-                    alpha = logoAlpha.value
-                },
-            tint = Colors.AppBlack
+                .size(Dimens.SplashScreenLogoSize150)
+                .graphicsLayer { alpha = logoAlpha.value }
         )
     }
 }
