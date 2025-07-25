6ef2299d2b0b103084950f477a61b065df4d6fb2
Svitlana Filippova <s.rubinets@gmail.com>
2025-07-25 09:36:43 +0300

    Доработан пин на карте

--- app/src/main/java/com/mandarinkafe/mandarin/features/address/map/presentation/ui/components/ChosenLocationPin.kt
+++ app/src/main/java/com/mandarinkafe/mandarin/features/address/map/presentation/ui/components/ChosenLocationPin.kt
@@ -1,8 +1,14 @@
 package com.mandarinkafe.mandarin.features.address.map.presentation.ui.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import com.mandarinkafe.mandarin.R
@@ -11,13 +17,50 @@ import com.mandarinkafe.mandarin.core.presentation.theme.Dimens
 
 @Composable
 fun ChosenLocationPin(
-    modifier: Modifier
+    modifier: Modifier,
+    isLoading: Boolean,
+    isError: Boolean,
+    addressFound: Boolean
 ) {
-    Icon(
-        painter = painterResource(R.drawable.ic_pin_dot),
-        tint = Colors.Orange,
-        contentDescription = null,
+    val iconRes = when {
+        isError -> R.drawable.ic_question
+        isLoading -> null
+        addressFound -> R.drawable.ic_home
+        else -> null
+    }
+    val indicatorSize = remember { Dimens.MapPinSize * 0.45f }
+    val indicatorOffsetY = remember { Dimens.MapPinSize * 0.09f }
+    Box(
         modifier = modifier
-            .size(Dimens.MapPinSize)
-    )
+            .size(Dimens.MapPinSize),
+        contentAlignment = Alignment.Center,
+
+        ) {
+        // Пин
+        Icon(
+            modifier = Modifier.fillMaxSize(),
+            painter = painterResource(R.drawable.ic_empty_pin),
+            tint = Colors.Orange,
+            contentDescription = null,
+        )
+        // Иконка внутри
+        iconRes?.let {
+            Icon(
+                modifier = Modifier.fillMaxSize(),
+                painter = painterResource(iconRes),
+                tint = Colors.Orange,
+                contentDescription = null,
+            )
+        }
+        // Индикатор загрузки
+        if (isLoading) {
+            CircularProgressIndicator(
+                color = Colors.Orange,
+                strokeWidth = Dimens.ProgressBarStroke6,
+                modifier = Modifier
+                    .size(indicatorSize)
+                    .offset(y = -indicatorOffsetY)
+            )
+        }
+    }
 }
