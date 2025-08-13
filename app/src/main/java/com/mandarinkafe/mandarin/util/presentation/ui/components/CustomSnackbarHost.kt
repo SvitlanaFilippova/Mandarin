9541ca34716e94936e4be4bcc584d6b7ee70dcbc
Svitlana Filippova <s.rubinets@gmail.com>
2025-08-13 09:32:32 +0300

    Кастомизирован Snackbar

--- app/src/main/java/com/mandarinkafe/mandarin/util/presentation/ui/components/CustomSnackbarHost.kt
+++ app/src/main/java/com/mandarinkafe/mandarin/util/presentation/ui/components/CustomSnackbarHost.kt
@@ -1,13 +1,19 @@
 package com.mandarinkafe.mandarin.util.presentation.ui.components
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Snackbar
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
+import com.mandarinkafe.mandarin.R
 import com.mandarinkafe.mandarin.core.presentation.theme.Colors
 import com.mandarinkafe.mandarin.core.presentation.theme.Dimens
 import com.mandarinkafe.mandarin.util.presentation.ui.components.buttons.ButtonWithText
@@ -26,6 +32,7 @@ fun CustomSnackbarHost(
                 containerColor = Colors.White.copy(alpha = 0.9f),
                 contentColor = Colors.AppBlack,
                 action = {
+                    // Кнопка действия
                     snackbarData.visuals.actionLabel?.let { actionLabel ->
                         ButtonWithText(
                             modifier = Modifier.padding(Dimens.MarginSmall8),
@@ -33,6 +40,18 @@ fun CustomSnackbarHost(
                             onClick = { snackbarData.performAction() })
                     }
                 },
+                dismissAction = {
+                    // Иконка закрытия для dismiss action
+                    IconButton(
+                        onClick = { snackbarData.dismiss() },
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Close,
+                            contentDescription = stringResource(R.string.close),
+                            tint = Colors.DarkGrey
+                        )
+                    }
+                }
             ) {
                 Text(
                     text = snackbarData.visuals.message,
