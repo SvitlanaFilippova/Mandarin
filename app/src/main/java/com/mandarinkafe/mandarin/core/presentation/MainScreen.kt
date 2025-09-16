15e30edd23039a531b951c5ef1f0e7dae0b897c1
Svitlana Filippova <s.rubinets@gmail.com>
2025-09-16 15:19:57 +0300

    Поля для ввода больше не перекрываются клавиатурой

--- app/src/main/java/com/mandarinkafe/mandarin/core/presentation/MainScreen.kt
+++ app/src/main/java/com/mandarinkafe/mandarin/core/presentation/MainScreen.kt
@@ -2,6 +2,8 @@ package com.mandarinkafe.mandarin.core.presentation
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
@@ -54,6 +56,9 @@ fun MainScreen() {
     val showBottomBar = !isSplash && !isInnerScreen
 
     Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .imePadding(),
         snackbarHost = { CustomSnackbarHost(snackbarHostState) },
         topBar = {
             AppTopBar(
