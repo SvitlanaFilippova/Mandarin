e518ad300f6cd533376abd7d21a90f77c91488a9
SvitlanaFilippova <s.rubinets@gmail.com>
2025-10-13 16:21:09 +0300

    refactor: Update BuildConfig with placeholder values
    - Replace hardcoded values with placeholder strings
    - Prepare for secure configuration setup

--- composeApp/src/commonMain/kotlin/com/mandarinkafe/mandarin/core/data/config/BuildConfig.kt
+++ composeApp/src/commonMain/kotlin/com/mandarinkafe/mandarin/core/data/config/BuildConfig.kt
@@ -1,11 +1,11 @@
 package com.mandarinkafe.mandarin.core.data.config
 
 object BuildConfig {
-    const val MAPKIT_API_KEY = "463cf8d9-966c-412d-bb4f-85f4be92beba"
-    const val IIKO_API_KEY = "3a901a233be740bea54bf0a38e1bfaa3"
-    const val TG_BOT_TOKEN = "8263905145:AAG_0adfiu5ZExwZyntLITQlvnpCp511K0I"
-    const val TG_CHANNEL_ID = "-1002905445250"
-    const val DEV_TG_CHAT_ID = "373620938"
+    const val MAPKIT_API_KEY = "YOUR_MAPKIT_API_KEY"
+    const val IIKO_API_KEY = "YOUR_IIKO_API_KEY"
+    const val TG_BOT_TOKEN = "YOUR_TG_BOT_TOKEN"
+    const val TG_CHANNEL_ID = "YOUR_TG_CHANNEL_ID"
+    const val DEV_TG_CHAT_ID = "YOUR_DEV_TG_CHAT_ID"
     const val SERVER_BASE_URL = "https://api.mandarin-kafe.ru/"
-    const val MANDARIN_API_KEY = "mandarin_7f3b1a9e5c8d2f4a6e0b1c9a8d5f3e2a"
+    const val MANDARIN_API_KEY = "YOUR_MANDARIN_API_KEY"
 }
