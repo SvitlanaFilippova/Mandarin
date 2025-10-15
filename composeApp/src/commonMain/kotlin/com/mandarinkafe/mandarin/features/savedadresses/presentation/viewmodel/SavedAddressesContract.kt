af53e2aef8f9deba1d1857f83e8949a16011a64f
SvitlanaFilippova <s.rubinets@gmail.com>
2025-10-15 13:38:23 +0300

    Перенос SavedAddressesViewModel на KMP
    Добавлено:
    - SavedAddressesViewModel с поддержкой KMP
    - SavedAddressesContract с типизированными событиями
    - Полная поддержка работы с сохраненными адресами
    
     Технические детали:
    - Простая структура без сложных зависимостей
    - Поддержка CRUD операций с адресами
    - Использование BaseContract для типизации
    - Автоматическое обновление списка после изменений

---
+++ composeApp/src/commonMain/kotlin/com/mandarinkafe/mandarin/features/savedadresses/presentation/viewmodel/SavedAddressesContract.kt
@@ -0,0 +1,25 @@
+package com.mandarinkafe.mandarin.features.savedadresses.presentation.viewmodel
+
+import com.mandarinkafe.mandarin.core.domain.models.Address
+import com.mandarinkafe.mandarin.util.presentation.BaseContract
+
+sealed interface SavedAddressesContract {
+    sealed interface SavedAddressesEvent : BaseContract.BaseEvent {
+        data object RefreshAddresses : SavedAddressesEvent
+        data object AddNewAddress : SavedAddressesEvent
+        data class EditAddress(val address: Address) : SavedAddressesEvent
+        data class RemoveAddress(val id: String) : SavedAddressesEvent
+    }
+
+    sealed interface SavedAddressesEffect : BaseContract.BaseEffect {
+
+        data object AddNewAddress : SavedAddressesEffect
+        data class EditAddress(val address: Address) : SavedAddressesEffect
+        data class ShowError(val message: String) : SavedAddressesEffect
+    }
+
+    data class SavedAddressesState(
+        val data: List<Address> = emptyList(),
+        val isLoading: Boolean? = null
+    ) : BaseContract.BaseState
+}
