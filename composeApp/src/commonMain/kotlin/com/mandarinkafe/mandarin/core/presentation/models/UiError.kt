c147b85bfa4e457205edcbe66b092e6e4fac0827
SvitlanaFilippova <s.rubinets@gmail.com>
2025-10-17 05:14:29 +0300

    Перенесен PlaceholderScreen из app модуля в composeApp
    - Создан PlaceholderScreen в composeApp с адаптацией под KMP
    - Изменена модель UiError: заменен image: String на imageRes: ImageResource

--- composeApp/src/commonMain/kotlin/com/mandarinkafe/mandarin/core/presentation/models/UiError.kt
+++ composeApp/src/commonMain/kotlin/com/mandarinkafe/mandarin/core/presentation/models/UiError.kt
@@ -1,58 +1,59 @@
 package com.mandarinkafe.mandarin.core.presentation.models
 
 import com.mandarinkafe.mandarin.MR
+import dev.icerock.moko.resources.ImageResource
 import dev.icerock.moko.resources.StringResource
 
 sealed class UiError(
     val msg: StringResource,
     val extraMsg: StringResource? = null,
-    val image: String
+    val imageRes: ImageResource
 ) {
     object NoInternet : UiError(
         MR.strings.error_no_internet,
         MR.strings.error_no_internet_extra,
-        "placeholder_no_internet"
+        MR.images.placeholder_no_internet
     )
 
     object OtherError : UiError(
         MR.strings.error_other,
         MR.strings.error_other_extra,
-        "placeholder_server_error"
+        MR.images.placeholder_server_error
     )
 
     object MenuEmpty : UiError(
         MR.strings.error_menu_empty,
         MR.strings.error_menu_empty_extra,
-        "placeholder_server_error"
+        MR.images.placeholder_server_error
     )
 
     object DataEmpty : UiError(
         MR.strings.error_data_empty,
         MR.strings.error_data_empty_extra,
-        "placeholder_server_error"
+        MR.images.placeholder_server_error
     )
 
     object FavoritesEmpty : UiError(
         MR.strings.error_favorites_empty,
         MR.strings.error_favorites_empty_extra,
-        "placeholder_empty_favorites"
+        MR.images.placeholder_empty_favorites
     )
 
     object CartEmpty : UiError(
         MR.strings.error_cart_empty,
         null,
-        "placeholder_empty_cart"
+        MR.images.placeholder_empty_cart
     )
 
     object SearchEmpty : UiError(
         MR.strings.error_search_empty,
         MR.strings.error_search_empty_extra,
-        "placeholder_nothing_found"
+        MR.images.placeholder_nothing_found
     )
 
     object EmptyOrderData : UiError(
         MR.strings.error_order_data_empty,
         MR.strings.error_order_data_empty_extra,
-        "placeholder_server_error"
+        MR.images.placeholder_server_error
     )
 }
