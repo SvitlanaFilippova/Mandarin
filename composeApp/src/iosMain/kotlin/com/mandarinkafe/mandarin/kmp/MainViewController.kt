8bc92b6ad625c6bed6d0b1bd40af8acdac6abccc
SvitlanaFilippova <s.rubinets@gmail.com>
2025-10-17 15:33:56 +0300

    Добавлена инициализация  Napier

--- composeApp/src/iosMain/kotlin/com/mandarinkafe/mandarin/kmp/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/mandarinkafe/mandarin/kmp/MainViewController.kt
@@ -2,12 +2,19 @@ package com.mandarinkafe.mandarin.kmp
 
 import androidx.compose.ui.window.ComposeUIViewController
 import com.mandarinkafe.mandarin.kmp.di.initKoinIOS
+import io.github.aakira.napier.DebugAntilog
+import io.github.aakira.napier.Napier
 import moe.tlaster.precompose.PreComposeApp
 
 fun MainViewController() = ComposeUIViewController {
     // Инициализируем Koin
     initKoinIOS()
-    
+
+    // Инициализируем Napier
+    Napier.base(DebugAntilog())
+
+    // TODO добавить инициализацию Mapkit SDK
+
     PreComposeApp {
         MainScreen()
     }
