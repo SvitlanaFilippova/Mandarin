204056f69043e039f1e6523087f7c0915f699af4
SvitlanaFilippova <s.rubinets@gmail.com>
2025-10-21 11:28:53 +0300

    Реализована kmp-функция OpenGeoLocation

---
+++ composeApp/src/iosMain/kotlin/com/mandarinkafe/mandarin/util/presentation/ui/components/intents/OpenGeoLocation.ios.kt
@@ -0,0 +1,21 @@
+package com.mandarinkafe.mandarin.util.presentation.ui.components.intents
+
+import androidx.compose.runtime.Composable
+import platform.Foundation.NSURL
+import platform.Foundation.NSURLComponents
+import platform.UIKit.UIApplication
+
+@Composable
+actual fun OpenGeoLocation(address: String) {
+    // Кодируем адрес для URL
+    val encodedAddress = address.replace(" ", "+")
+    
+    // Используем Apple Maps URL scheme
+    val urlString = "http://maps.apple.com/?q=$encodedAddress"
+    val url = NSURL.URLWithString(urlString)
+    
+    if (url != null && UIApplication.sharedApplication.canOpenURL(url)) {
+        UIApplication.sharedApplication.openURL(url)
+    }
+}
+
