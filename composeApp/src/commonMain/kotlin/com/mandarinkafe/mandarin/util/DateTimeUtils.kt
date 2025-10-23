7d204108c02fcc001b4c69468c0ec728c5d447fb
SvitlanaFilippova <s.rubinets@gmail.com>
2025-10-23 15:45:39 +0300

    Исправлена функция DateTimeUtils - toHumanDateTimeOrNull

--- composeApp/src/commonMain/kotlin/com/mandarinkafe/mandarin/util/DateTimeUtils.kt
+++ composeApp/src/commonMain/kotlin/com/mandarinkafe/mandarin/util/DateTimeUtils.kt
@@ -1,34 +1,34 @@
 package com.mandarinkafe.mandarin.util
 
-import io.github.aakira.napier.Napier
 import kotlinx.datetime.LocalDateTime
-import kotlinx.datetime.format
-import kotlinx.datetime.format.DateTimeComponents
-import kotlinx.datetime.format.char
+import kotlinx.datetime.number
 
 object DateTimeUtils {
     fun String?.toHumanDateTimeOrNull(): String? = try {
         this?.let {
-            // Парсим формат "yyyy-MM-dd HH:mm:ss.SSS"
-            val dateTime = LocalDateTime.parse(it.substringBefore('.'))
+            if (it.isBlank()) return null
             
+            // Парсим формат "yyyy-MM-dd HH:mm:ss.SSS" или "yyyy-MM-dd HH:mm:ss"
+            val dateString = if (it.contains('.')) {
+                it.substringBefore('.')
+            } else {
+                it
+            }
+
+            // Заменяем пробел на 'T' для совместимости с ISO форматом
+            val isoDateString = dateString.replace(" ", "T")
+            val dateTime = LocalDateTime.parse(isoDateString)
+
             // Форматируем в "HH:mm, dd.MM.yyyy"
             val hour = dateTime.hour.toString().padStart(2, '0')
             val minute = dateTime.minute.toString().padStart(2, '0')
-            val day = dateTime.dayOfMonth.toString().padStart(2, '0')
-            val month = dateTime.monthNumber.toString().padStart(2, '0')
+            val day = dateTime.day.toString().padStart(2, '0')
+            val month = dateTime.month.number.toString().padStart(2, '0')
             val year = dateTime.year
-            
+
             "$hour:$minute, $day.$month.$year"
         }
     } catch (e: Exception) {
-        Napier.e("error: $e")
         null
     }
 }
-
-
-
-
-
-
