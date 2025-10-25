fbb1291a9af86c27088b92ec1d5e8489188bc605
SvitlanaFilippova <s.rubinets@gmail.com>
2025-10-25 07:58:05 +0300

    fix: исправлена проблема с обновлением списка заказов в OrdersHistoryScreen
    - Изменен singleOf на factoryOf для OrdersHistoryViewModel в DI модуле
    - Теперь при каждом заходе в OrdersHistoryScreen создается новый ViewModel
    - Список заказов корректно обновляется при перезаходе
    
    Исправления:
    - refreshData() вызывается при создании каждого нового OrdersHistoryScreen
    - Данные о заказах обновляются актуально

--- composeApp/src/commonMain/kotlin/com/mandarinkafe/mandarin/features/ordershistory/di/OrdersHistoryModule.kt
+++ composeApp/src/commonMain/kotlin/com/mandarinkafe/mandarin/features/ordershistory/di/OrdersHistoryModule.kt
@@ -15,6 +15,7 @@ import com.mandarinkafe.mandarin.features.ordershistory.domain.impl.SaveOrderToH
 import com.mandarinkafe.mandarin.features.ordershistory.presentation.viewmodel.OrdersHistoryViewModel
 import com.mandarinkafe.mandarin.shared.database.AppDatabase
 import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.factoryOf
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
