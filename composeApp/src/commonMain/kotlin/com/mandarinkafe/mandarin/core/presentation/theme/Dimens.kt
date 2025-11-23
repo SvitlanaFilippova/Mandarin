5aa610f7c35631b15e2c8547cf030c27183bd9a9
SvitlanaFilippova <s.rubinets@gmail.com>
2025-11-23 07:11:39 +0300

    Обновлён внешний вид селекторов типа заказа и оплаты на экране оформления заказа

--- composeApp/src/commonMain/kotlin/com/mandarinkafe/mandarin/core/presentation/theme/Dimens.kt
+++ composeApp/src/commonMain/kotlin/com/mandarinkafe/mandarin/core/presentation/theme/Dimens.kt
@@ -60,6 +60,8 @@ object Dimens {
     val BannerIndicatorActiveWidth32 = 32.dp
     val BannerIndicatorInactiveWidth8 = 8.dp
     val BannerIndicatorSize4 = 4.dp
+    val DeliveryChooserHeight = 56.dp
+    val PaymentChooserHeight = 68.dp
     val PriceMinWidthForCustomizeItem = 64.dp
     val CodeBoxWidth = 36.dp
     val CodeBoxHeight = 48.dp
--- composeApp/src/commonMain/kotlin/com/mandarinkafe/mandarin/features/order/presentation/ui/components/DeliveryTypeChooser.kt
+++ composeApp/src/commonMain/kotlin/com/mandarinkafe/mandarin/features/order/presentation/ui/components/DeliveryTypeChooser.kt
@@ -1,23 +1,19 @@
 package com.mandarinkafe.mandarin.features.order.presentation.ui.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.SegmentedButton
-import androidx.compose.material3.SegmentedButtonColors
-import androidx.compose.material3.SegmentedButtonDefaults
-import androidx.compose.material3.SingleChoiceSegmentedButtonRow
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.mandarinkafe.mandarin.core.presentation.theme.Colors
+import androidx.compose.ui.semantics.Role
 import com.mandarinkafe.mandarin.core.presentation.theme.Dimens
 import com.mandarinkafe.mandarin.features.order.domain.models.DeliveryType
 import com.mandarinkafe.mandarin.features.order.presentation.models.UiDeliveryType
 import com.mandarinkafe.mandarin.features.order.presentation.models.toDomain
-import com.mandarinkafe.mandarin.util.presentation.ui.components.SegmentedButtonLabel
-import dev.icerock.moko.resources.compose.painterResource
-import dev.icerock.moko.resources.compose.stringResource
 
 @Composable
 fun DeliveryTypeChooser(
@@ -29,52 +25,38 @@ fun DeliveryTypeChooser(
     val deliveryTypes = remember { UiDeliveryType.entries.toList() }
 
 
-    SingleChoiceSegmentedButtonRow(modifier = Modifier.fillMaxWidth()) {
-        deliveryTypes.forEachIndexed { index, item ->
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = Dimens.MarginSmall8),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(Dimens.MarginSmall8)
+    ) {
+        deliveryTypes.forEach { item ->
             val selected = item.toDomain() == chosen
-            val borderColor = when {
-                selected -> Colors.Orange
-                isError && chosen == null -> Colors.Red
-                else -> Colors.AppBlack
-            }
 
             val isDelivery = item == UiDeliveryType.DELIVERY
             val itemEnabled = if (isDelivery) deliveryEnabled else true
-
-            SegmentedButton(
-                modifier = Modifier.height(Dimens.BigButtonWithTextHeight),
-                shape = SegmentedButtonDefaults.itemShape(
-                    index = index,
-                    count = deliveryTypes.size,
-                    baseShape = RoundedCornerShape(Dimens.CornerRadius8)
-                ),
-                onClick = { onDeliverySelected(item.toDomain()) },
-                selected = selected,
-                enabled = itemEnabled,
-                colors = SegmentedButtonColors(
-                    activeContainerColor = Colors.DarkGrey,
-                    activeContentColor = Colors.Orange,
-                    activeBorderColor = borderColor,
-                    inactiveContainerColor = Colors.DarkGrey,
-                    inactiveContentColor = Colors.White,
-                    inactiveBorderColor = borderColor,
-                    disabledActiveContainerColor = Colors.AppBlack,
-                    disabledActiveContentColor = Colors.DarkGrey,
-                    disabledActiveBorderColor = Colors.AppBlack,
-                    disabledInactiveContainerColor = Colors.DarkGrey,
-                    disabledInactiveContentColor = Colors.AppBlack,
-                    disabledInactiveBorderColor = Colors.AppBlack,
-                ),
-                icon = { },
-                label = {
-                    SegmentedButtonLabel(
-                        selected = selected,
-                        name = stringResource(item.nameRes),
-                        icon = painterResource(item.iconRes),
-                        isEnabled = itemEnabled
+            val modifier = if (itemEnabled) {
+                Modifier.weight(1f)
+                    .clickable(
+                        onClick = { onDeliverySelected(item.toDomain()) },
+                        role = Role.Button
                     )
-                }
+            } else {
+                Modifier
+                    .weight(1f)
+            }
+
+            OrderTypeChooserHorizontalItem(
+                modifier = modifier,
+                selected = selected,
+                label = item.nameRes,
+                icon = item.iconRes,
+                enabled = itemEnabled,
+                isError = isError && chosen == null
             )
         }
+
     }
 }
