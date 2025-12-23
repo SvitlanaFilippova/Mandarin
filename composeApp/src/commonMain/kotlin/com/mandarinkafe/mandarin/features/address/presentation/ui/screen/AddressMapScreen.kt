dc03e0493171ef70d03c8ab771536fda080c7de6
SvitlanaFilippova <s.rubinets@gmail.com>
2025-12-23 11:01:56 +0300

    Добавлена подсказка к экрану добавления адреса: "Введите адрес текстом или выберите на карте"

--- composeApp/src/commonMain/kotlin/com/mandarinkafe/mandarin/features/address/presentation/ui/screen/AddressMapScreen.kt
+++ composeApp/src/commonMain/kotlin/com/mandarinkafe/mandarin/features/address/presentation/ui/screen/AddressMapScreen.kt
@@ -5,21 +5,25 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarResult
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import com.mandarinkafe.mandarin.util.presentation.ui.components.intents.getContextForSettings
 import androidx.navigation.NavController
 import com.mandarinkafe.mandarin.MR
 import com.mandarinkafe.mandarin.core.domain.models.Address
 import com.mandarinkafe.mandarin.core.presentation.theme.Dimens
+import com.mandarinkafe.mandarin.core.presentation.theme.Typography
 import com.mandarinkafe.mandarin.features.address.presentation.ui.components.HandleAddressEffects
 import com.mandarinkafe.mandarin.features.address.presentation.ui.components.SearchByTextResults
 import com.mandarinkafe.mandarin.features.address.presentation.viewmodel.AddressContract
@@ -27,17 +31,15 @@ import com.mandarinkafe.mandarin.features.map.MapCameraController
 import com.mandarinkafe.mandarin.features.order.presentation.ui.components.LocationIcon
 import com.mandarinkafe.mandarin.shared.presentation.viewmodel.rememberAddressViewModel
 import com.mandarinkafe.mandarin.util.Constants.MIN_LINES_FOR_ADDRESS_INPUT
-import androidx.compose.material3.SnackbarDuration
-import androidx.compose.material3.SnackbarResult
-import androidx.compose.runtime.rememberCoroutineScope
 import com.mandarinkafe.mandarin.util.presentation.LocalSnackbarHostState
-import com.mandarinkafe.mandarin.util.presentation.ui.components.intents.openAppSettings
-import kotlinx.coroutines.launch
 import com.mandarinkafe.mandarin.util.presentation.ui.components.ScreenTitleWithBackButton
 import com.mandarinkafe.mandarin.util.presentation.ui.components.buttons.ButtonWithText
+import com.mandarinkafe.mandarin.util.presentation.ui.components.intents.getContextForSettings
+import com.mandarinkafe.mandarin.util.presentation.ui.components.intents.openAppSettings
 import com.mandarinkafe.mandarin.util.presentation.ui.components.map.RequestLocationPermission
 import com.mandarinkafe.mandarin.util.presentation.ui.components.textfields.MyTextField
 import dev.icerock.moko.resources.compose.stringResource
+import kotlinx.coroutines.launch
 
 
 @Composable
@@ -105,19 +107,20 @@ fun AddressMapScreen(
         openAppSettings(context)
     }
 
-    val onShowSnackbarWithAction: (String, String, () -> Unit) -> Unit = { message, actionLabel, onAction ->
-        coroutineScope.launch {
-            val result = snackbarHostState.showSnackbar(
-                message = message,
-                duration = SnackbarDuration.Long,
-                withDismissAction = true,
-                actionLabel = actionLabel
-            )
-            if (result == SnackbarResult.ActionPerformed) {
-                onAction()
+    val onShowSnackbarWithAction: (String, String, () -> Unit) -> Unit =
+        { message, actionLabel, onAction ->
+            coroutineScope.launch {
+                val result = snackbarHostState.showSnackbar(
+                    message = message,
+                    duration = SnackbarDuration.Long,
+                    withDismissAction = true,
+                    actionLabel = actionLabel
+                )
+                if (result == SnackbarResult.ActionPerformed) {
+                    onAction()
+                }
             }
         }
-    }
 
 
     Column(
@@ -128,6 +131,14 @@ fun AddressMapScreen(
         ScreenTitleWithBackButton(
             name = stringResource(MR.strings.address_screen_title),
             onBackClick = { navController.popBackStack() }
+        )
+
+        Text(
+            text = stringResource(MR.strings.choose_address_by_text),
+            style = Typography.SmallLightTextStyle,
+            modifier = Modifier.padding(
+                Dimens.MarginSuperSmall4
+            )
         )
 
         // Строка с адресом
