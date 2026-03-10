51e26f701c0b5c504b0c8008f1517b385926cd99
SvitlanaFilippova <s.rubinets@gmail.com>
2026-03-10 11:23:33 +0300

    Добавлены альтернативные пояснения к статусу заказа для самовывоза (без упоминаний курьера)

--- composeApp/src/commonMain/kotlin/com/mandarinkafe/mandarin/features/orderinfo/presentation/ui/models/UiDeliveryStatus.kt
+++ composeApp/src/commonMain/kotlin/com/mandarinkafe/mandarin/features/orderinfo/presentation/ui/models/UiDeliveryStatus.kt
@@ -6,75 +6,101 @@ import dev.icerock.moko.resources.ImageResource
 import dev.icerock.moko.resources.StringResource
 
 enum class UiDeliveryStatus(
-    val apiName: String,
     val nameRes: StringResource,
     val extraTextResId: StringResource?,
     val iconRes: ImageResource,
 ) {
     UNCONFIRMED(
-        "Unconfirmed",
         MR.strings.delivery_status_unconfirmed,
         MR.strings.delivery_status_extra_unconfirmed,
         MR.images.ic_timer
     ),
     WAIT_COOKING(
-        "WaitCooking",
         MR.strings.delivery_status_wait_cooking,
         MR.strings.delivery_status_extra_wait_cooking,
         MR.images.ic_timer
     ),
     READY_FOR_COOKING(
-        "ReadyForCooking",
         MR.strings.delivery_status_wait_cooking,
         MR.strings.delivery_status_extra_wait_cooking,
         MR.images.ic_timer
     ),
     COOKING_STARTED(
-        "CookingStarted",
         MR.strings.delivery_status_cooking_started,
         MR.strings.delivery_status_extra_cooking_started,
         MR.images.ic_chef_hat
     ),
     COOKING_COMPLETED(
-        "CookingCompleted",
         MR.strings.delivery_status_cooking_completed,
         MR.strings.delivery_status_extra_cooking_completed,
         MR.images.ic_hand_meal
     ),
     WAITING(
-        "Waiting",
         MR.strings.delivery_status_waiting,
         MR.strings.delivery_status_extra_waiting,
         MR.images.ic_orders
     ),
+
     ON_WAY(
-        "OnWay",
         MR.strings.delivery_status_on_way,
         MR.strings.delivery_status_extra_on_way,
         MR.images.ic_courier
     ),
     DELIVERED(
-        "Delivered",
         MR.strings.delivery_status_delivered,
         MR.strings.delivery_status_extra_delivered,
         MR.images.ic_cottage
     ),
     CLOSED(
-        "Closed",
         MR.strings.delivery_status_closed,
         MR.strings.delivery_status_extra_closed,
         MR.images.ic_check
     ),
     CANCELLED(
-        "Cancelled",
         MR.strings.delivery_status_cancelled,
         null,
         MR.images.ic_no_food
-    )
+    ),
+    COOKING_COMPLETED_SELFPICKUP(
+        MR.strings.delivery_status_cooking_completed,
+        MR.strings.delivery_status_extra_cooking_completed_selfpickup,
+        MR.images.ic_hand_meal
+    ),
+    WAITING_SELFPICKUP(
+        MR.strings.delivery_status_waiting,
+        MR.strings.delivery_status_extra_waiting_selfpickup,
+        MR.images.ic_orders
+    ),
 }
 
-fun DeliveryStatus.toUi(): UiDeliveryStatus {
-    return UiDeliveryStatus.entries.firstOrNull { it.apiName == this.apiName }
-        ?: error("Unknown DeliveryStatus: $this")
+
+private fun DeliveryStatus.baseUi(): UiDeliveryStatus =
+    when (this) {
+        DeliveryStatus.UNCONFIRMED -> UiDeliveryStatus.UNCONFIRMED
+        DeliveryStatus.WAIT_COOKING -> UiDeliveryStatus.WAIT_COOKING
+        DeliveryStatus.READY_FOR_COOKING -> UiDeliveryStatus.READY_FOR_COOKING
+        DeliveryStatus.COOKING_STARTED -> UiDeliveryStatus.COOKING_STARTED
+        DeliveryStatus.COOKING_COMPLETED -> UiDeliveryStatus.COOKING_COMPLETED
+        DeliveryStatus.WAITING -> UiDeliveryStatus.WAITING
+        DeliveryStatus.ON_WAY -> UiDeliveryStatus.ON_WAY
+        DeliveryStatus.DELIVERED -> UiDeliveryStatus.DELIVERED
+        DeliveryStatus.CLOSED -> UiDeliveryStatus.CLOSED
+        DeliveryStatus.CANCELLED -> UiDeliveryStatus.CANCELLED
+    }
+
+fun DeliveryStatus.toUi(isDelivery: Boolean? = null): UiDeliveryStatus {
+    if (isDelivery == false) {
+        when (this) {
+            DeliveryStatus.COOKING_COMPLETED ->
+                return UiDeliveryStatus.COOKING_COMPLETED_SELFPICKUP
+
+            DeliveryStatus.WAITING ->
+                return UiDeliveryStatus.WAITING_SELFPICKUP
+
+            else -> {}
+        }
+    }
+
+    return baseUi()
 }
 
