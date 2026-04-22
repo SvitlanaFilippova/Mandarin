4fcf97e2d9aafbb0dd8cc3bd02744b0890d8b995
SvitlanaFilippova <s.rubinets@gmail.com>
2026-04-22 06:15:16 +0300

    Добавлен clearFocusOnTap на рут контейнер для закрытия клавиатуры по тапу вне её зоны

--- composeApp/src/commonMain/kotlin/com/mandarinkafe/mandarin/util/presentation/Extensions.kt
+++ composeApp/src/commonMain/kotlin/com/mandarinkafe/mandarin/util/presentation/Extensions.kt
@@ -1,5 +1,10 @@
 package com.mandarinkafe.mandarin.util.presentation
 
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
 import com.mandarinkafe.mandarin.core.domain.models.GeoPoint
 import kotlin.math.abs
 
@@ -8,4 +13,16 @@ fun GeoPoint.isSameAs(other: GeoPoint, epsilon: Double = 1e-6): Boolean {
     val otherPoint = other
     return abs(point.latitude - otherPoint.latitude) < epsilon &&
             abs(point.longitude - otherPoint.longitude) < epsilon
+}
+
+fun Modifier.clearFocusOnTap(): Modifier = composed {
+    val focusManager = LocalFocusManager.current
+
+    pointerInput(Unit) {
+        detectTapGestures(
+            onTap = {
+                focusManager.clearFocus()
+            }
+        )
+    }
 }
